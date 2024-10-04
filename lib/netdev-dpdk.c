kevintraynor/ovs

commit afee281f7f4f4b477e5c12bf18fe00d097ce8e96

netdev-dpdk: Fix dpdk_watchdog failure to quiesce.
Fix issue whereby vhost_thread is waiting for dpdk_watchdog
thread to quiesce and at the same time dpdk_watchdog thread
is waiting for vhost_thread to give up dpdk_mutex.

Reported-by: Patrik Andersson R <patrik.r.andersson@ericsson.com>
Signed-off-by: Patrik Andersson R <patrik.r.andersson@ericsson.com>
Signed-off-by: Kevin Traynor <kevin.traynor@intel.com>
Signed-off-by: Daniele Di Proietto <diproiettod@vmware.com>

lib/netdev-dpdk.c
@@ -1904,6 +1904,7 @@ static void
 destroy_device(volatile struct virtio_net *dev)
 {
     struct netdev_dpdk *vhost_dev;
+    bool exists = false;
 
     ovs_mutex_lock(&dpdk_mutex);
     LIST_FOR_EACH (vhost_dev, list_node, &dpdk_list) {
@@ -1912,24 +1913,32 @@ destroy_device(volatile struct virtio_net *dev)
             ovs_mutex_lock(&vhost_dev->mutex);
             dev->flags &= ~VIRTIO_DEV_RUNNING;
             ovsrcu_set(&vhost_dev->virtio_dev, NULL);
+            exists = true;
             ovs_mutex_unlock(&vhost_dev->mutex);
-
-            /*
-             * Wait for other threads to quiesce before
-             * setting the virtio_dev to NULL.
-             */
-            ovsrcu_synchronize();
-            /*
-             * As call to ovsrcu_synchronize() will end the quiescent state,
-             * put thread back into quiescent state before returning.
-             */
-            ovsrcu_quiesce_start();
+            break;
         }
     }
+
     ovs_mutex_unlock(&dpdk_mutex);
 
-    VLOG_INFO("vHost Device '%s' %"PRIu64" has been removed", dev->ifname,
-              dev->device_fh);
+    if (exists == true) {
+        /*
+         * Wait for other threads to quiesce after setting the 'virtio_dev'
+         * to NULL, before returning.
+         */
+        ovsrcu_synchronize();
+        /*
+         * As call to ovsrcu_synchronize() will end the quiescent state,
+         * put thread back into quiescent state before returning.
+         */
+        ovsrcu_quiesce_start();
+        VLOG_INFO("vHost Device '%s' %"PRIu64" has been removed", dev->ifname,
+                  dev->device_fh);
+    } else {
+        VLOG_INFO("vHost Device '%s' %"PRIu64" not found", dev->ifname,
+                  dev->device_fh);
+    }
+
 }
 
 struct virtio_net *

AUTHORS
@@ -360,6 +360,7 @@ Niels van Adrichem      N.L.M.vanAdrichem@tudelft.nl
 Niklas Andersson        nandersson@nicira.com
 Pankaj Thakkar          thakkar@nicira.com
 Pasi Kärkkäinen         pasik@iki.fi
+Patrik Andersson R      patrik.r.andersson@ericsson.com
 Paulo Cravero           pcravero@as2594.net
 Pawan Shukla            shuklap@vmware.com
 Peter Amidon            peter@picnicpark.org

commit 6f513d414d511f840c4446b9e940a5d3238984f0

netdev-dpdk: Postpone vhost driver unregister.
If there is a device notification from the vhost-event thread
that calls destroy_device() and the user deletes the vhost port at
the same time, there is a possibility of a deadlock occuring. e.g.

vhost-event thread is blocking on the ovsrcu_synchronize(), waiting
for main thread to queisce, i.e.

0x0000000003690c09 in ovsrcu_synchronize () at lib/ovs-rcu.c:237
0x00000000037162fa in destroy_device (vid=0) at lib/netdev-dpdk.c:4919
0x0000000003238a12 in vhost_destroy_device_notify (dev=0x14ba693c0)
at ../lib/vhost/vhost.c:756

While main thread is looping in rte_vhost_driver_unregister(), waiting
for vhost-event callback to finish, i.e.:

0x00000000032336e8 in rte_vhost_driver_unregister (path=0xd217a30
"/tmp/vhost0") at ../lib/vhost/socket.c:1075
0x000000000370d5f3 in dpdk_vhost_driver_unregister (dev=0x17d6e8b40,
vhost_id=0xd217a30 "/tmp/vhost0") at lib/netdev-dpdk.c:1811
0x000000000370d709 in netdev_dpdk_vhost_destruct
(netdev=0x17d6e8bc0) at lib/netdev-dpdk.c:1843

In order to prevent this situation, use ovsrcu to postpone the call to
rte_vhost_driver_unregister(). This means that main thread will not
block and will quiesce, allowing ovsrcu_synchornize() in destroy_device()
to complete.

Fixes: afee281f7f4f ("netdev-dpdk: Fix dpdk_watchdog failure to quiesce.")
Reported-by: Xinxin Zhao <15957197901@163.com>
Signed-off-by: Kevin Traynor <ktraynor@redhat.com>

lib/netdev-dpdk.c
@@ -200,6 +200,13 @@ static const struct rte_vhost_device_ops virtio_net_device_ops =
     .destroy_connection = destroy_connection,
 };
 
+/* Information used for postponed vhost driver unregistering. */
+struct vhost_unregister_info {
+    char *vhost_id;
+    char *name;
+    bool is_server;
+};
+
 /* Custom software stats for dpdk ports */
 struct netdev_dpdk_sw_stats {
     /* No. of retries when unable to transmit. */
@@ -1801,13 +1808,42 @@ netdev_dpdk_destruct(struct netdev *netdev)
 /* rte_vhost_driver_unregister() can call back destroy_device(), which will
  * try to acquire 'dpdk_mutex' and possibly 'dev->mutex'.  To avoid a
  * deadlock, none of the mutexes must be held while calling this function. */
-static int
-dpdk_vhost_driver_unregister(struct netdev_dpdk *dev OVS_UNUSED,
-                             char *vhost_id)
+static void
+dpdk_vhost_driver_unregister(struct vhost_unregister_info *vhost_info)
     OVS_EXCLUDED(dpdk_mutex)
-    OVS_EXCLUDED(dev->mutex)
 {
-    return rte_vhost_driver_unregister(vhost_id);
+    char *vhost_id = vhost_info->vhost_id;
+    char *name = vhost_info->name;
+
+    if (rte_vhost_driver_unregister(vhost_id)) {
+        VLOG_ERR("%s: Unable to unregister vhost driver for socket '%s'.\n",
+                 name, vhost_id);
+    } else if (vhost_info->is_server) {
+        /* OVS server mode - remove this socket from list for deletion */
+        fatal_signal_remove_file_to_unlink(vhost_id);
+    }
+    free(vhost_id);
+    free(name);
+    free(vhost_info);
+}
+
+/* Postpone calling rte_vhost_driver_unregister() so it is not called from the
+ * main thread otherwise it may deadlock with a device notification calling
+ * destroy_device(). i.e. rte_vhost_driver_unregister() from main thread
+ * blocks waiting for destroy_device() to complete, destroy_device() blocks
+ * waiting for main thread to quiesce. */
+static void
+dpdk_vhost_driver_unregister_postpone(const char *vhost_id, const char *name,
+                                      bool is_server)
+{
+    struct vhost_unregister_info *vhost_info;
+
+    vhost_info = xmalloc(sizeof *vhost_info);
+    vhost_info->vhost_id = xstrdup(vhost_id);
+    vhost_info->name = xstrdup(name);
+    vhost_info->is_server = is_server;
+
+    ovsrcu_postpone(dpdk_vhost_driver_unregister, vhost_info);
 }
 
 static void
@@ -1836,18 +1872,13 @@ netdev_dpdk_vhost_destruct(struct netdev *netdev)
     ovs_mutex_unlock(&dpdk_mutex);
 
     if (!vhost_id) {
-        goto out;
+        free(vhost_id);
+        return;
     }
 
-    if (dpdk_vhost_driver_unregister(dev, vhost_id)) {
-        VLOG_ERR("%s: Unable to unregister vhost driver for socket '%s'.\n",
-                 netdev->name, vhost_id);
-    } else if (!(dev->vhost_driver_flags & RTE_VHOST_USER_CLIENT)) {
-        /* OVS server mode - remove this socket from list for deletion */
-        fatal_signal_remove_file_to_unlink(vhost_id);
-    }
-out:
-    free(vhost_id);
+    dpdk_vhost_driver_unregister_postpone(vhost_id, netdev_get_name(netdev),
+                                          !(dev->vhost_driver_flags
+                                          & RTE_VHOST_USER_CLIENT));
 }
 
 static void
@@ -6284,7 +6315,10 @@ netdev_dpdk_vhost_client_reconfigure(struct netdev *netdev)
     ovs_mutex_unlock(&dev->mutex);
 
     if (unregister) {
-        dpdk_vhost_driver_unregister(dev, vhost_id);
+        dpdk_vhost_driver_unregister_postpone(vhost_id,
+                                              netdev_get_name(netdev),
+                                              !(dev->vhost_driver_flags
+                                              & RTE_VHOST_USER_CLIENT));
     }
 
     ovs_mutex_lock(&dev->mutex);